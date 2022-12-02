7f2423587b4639a601d09d2482aa0995cab96fdc
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-02 10:09:33 +0800

    yid

---
+++ src/main/java/com/yid/controller/AliceServlet.java
@@ -0,0 +1,12 @@
+package com.yid.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/demo/alice")
+public class AliceServlet extends HttpServlet {
+    
+    
+    
+    
+}

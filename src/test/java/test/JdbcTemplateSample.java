d2966d6971c8a4e3572928bd60aad7e6c8361941
vincenttuan <vincentjava@yahoo.com.tw>
2022-12-23 13:19:14 +0800

    yid

--- src/test/java/test/JdbcTemplateSample.java
+++ src/test/java/test/JdbcTemplateSample.java
@@ -1,15 +1,8 @@
 package test;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import com.yid.repository.YIDConnection;
 import com.yid.repository.guestbook.entity.GuestBook;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
 import java.util.List;
-import java.util.logging.Logger;
-import javax.sql.DataSource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
